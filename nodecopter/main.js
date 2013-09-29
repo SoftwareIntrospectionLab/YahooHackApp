6515db03f340ebf59c6d2be7ada54dd2de634bb7
Brandy Dettmer <bdettmer@gmail.com>
2013-09-29 00:41:37 -0700

    it works: fly's high based on sensor input. we need to get it to stop now

--- nodecopter/main.js
+++ nodecopter/main.js
@@ -2,13 +2,16 @@
 require('johnny-five')
 require('ar-drone')
 
+var dist = 0;
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 var five = require("johnny-five"),
     board, ping;
 
-board = new five.Board();
 
+console.log("1");
+board = new five.Board();
+console.log("2");
 board.on("ready", function() {
 
     console.log("yay! go nodedrone!");
@@ -17,54 +20,57 @@ board.on("ready", function() {
 
     ping = new five.Ping(6);
 
+    console.log("3");
     ping.on("data", function( err, value ) {
 	console.log( "data", value );
     });
 
     ping.on("change", function( err, value ) {
 	console.log( typeof this.inches );
+	dist = this.inches;
+        dist = dist * 1000;
 	console.log( "Object is " + this.inches + "inches away" );
+	
+	client.takeoff();
+
+	client.up(.2);
+
+	client
+	    .after(3000, function() {
+		console.log("5");
+		this.stop();
+		this.land();
+ });
+
     });
 });
 
 
+/*
 client
-   .after(4000, function() {
-   console.log("in function");
+   .after(2000, function() {
+   console.log("4");
+   console.log("in function dist=" + dist);
  });
 
 
-/*
+
 client.takeoff();
 
 client.up(.9)
 
 client
-   .after(40000, function() {
+   .after(3000, function() {
+       console.log("5");
        this.stop();
-   });
-
-
-//client.animate('flipAhead', 1000);
-
-client.animate('flipBehind', 2000);
-
-//client.animate('flipAhead', 3000);
-
-client
-  .after(5000, function() {
-    this.clockwise(5.5);
-  })
-  .after(3000, function() {
-    this.stop();
-    this.land();
-  });
-
+       this.land();
+ });
 */
 
 // only if we kill the script, this is an auto-emergency brake
 process.on("SIGINT", function() {
     console.log("landing drone");
+    console.log("6");
     console.log( "Object is " + this.inches + "inches away" ); 
    // client.land();
 });
