ab931f6f43ec69b060be71552c5c429bec9de7a4
Brandy Dettmer <bdettmer@gmail.com>
2013-09-28 21:43:08 -0700

    added test drivers for johnny-five and ping ultrasonic sensor

--- nodecopter/main.js
+++ nodecopter/main.js
@@ -4,6 +4,14 @@ require('ar-drone')
 
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
+var five = require("johnny-five"),
+     board = new five.Board();
+
+board.on("ready", function() {
+
+   (new five.Led(13)).strobe();
+});
+
 
 //var pngStream = client.getPngStream();
 
